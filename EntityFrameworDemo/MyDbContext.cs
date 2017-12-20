333c541d7c78a73f20179c91de7eb5ce518c4b53
starrysky-pfw <1130281533@qq.com>
2017-12-20 23:07:56 +0800

    create entity framework demo

---
+++ EntityFrameworDemo/MyDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace EntityFrameworDemo
+{
+    /// <summary>
+    /// 数据库上下文
+    /// </summary>
+    public class MyDbContext : DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
