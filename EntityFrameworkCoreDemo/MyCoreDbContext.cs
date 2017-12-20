1744b5933d5b0cb7c894f7ad1cc80efa7a01f7f9
starrysky-pfw <1130281533@qq.com>
2017-12-20 23:18:02 +0800

    Create Entity Framework Core Demo

---
+++ EntityFrameworkCoreDemo/MyCoreDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCoreDemo
+{
+    public class MyCoreDbContext : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
