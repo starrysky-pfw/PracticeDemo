5ba98f790597220cd13cc64bf2cc53dea5a9584e
starrysky-pfw <1130281533@qq.com>
2017-12-21 23:28:03 +0800

    Update Connection

--- EntityFrameworkCoreDemo/MyCoreDbContext.cs
+++ EntityFrameworkCoreDemo/MyCoreDbContext.cs
@@ -7,11 +7,29 @@ using System.Threading.Tasks;
 
 namespace EntityFrameworkCoreDemo
 {
+    /// <summary>
+    /// 数据库上下文
+    /// </summary>
     public class MyCoreDbContext : DbContext
     {
+        public MyCoreDbContext(DbContextOptions<MyCoreDbContext> options) : base(options)
+        {
+
+        }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+        }
+
+        /// <summary>
+        /// 配置 
+        /// </summary>
+        /// <param name="optionsBuilder"></param>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.;Database=EFCodeAdvance;uid=sa;pwd=sql2012;");
+            base.OnConfiguring(optionsBuilder);
         }
     }
 }
