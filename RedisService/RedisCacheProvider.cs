b2466387fbd22ce74033e03e845a54aa91c7d8ce
starrysky-pfw <1130281533@qq.com>
2018-01-22 22:47:36 +0800

    StackExchange.Redis

---
+++ RedisService/RedisCacheProvider.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace RedisService
+{
+    public class RedisCacheProvider
+    {
+        static ConnectionMultiplexer connectionMultiplexer;
+
+        public static ConnectionMultiplexer RedisManager()
+        {
+            if (connectionMultiplexer == null || !connectionMultiplexer.IsConnected)
+            {
+                connectionMultiplexer = ConnectionMultiplexer.Connect(new ConfigurationOptions() { ClientName = "", Password = "" });
+            }
+            return connectionMultiplexer;
+        }
+    }
+
+    public class RedisService
+    {
+        private int db = 0; //默认数据库
+
+        public RedisService DB(int db)
+        {
+            RedisCacheProvider.RedisManager().GetDatabase(db);
+            return this;
+        }
+
+
+        public void SetStr()
+        {
+            RedisCacheProvider.RedisManager().GetDatabase().StringSet("", "", TimeSpan.FromSeconds(100));
+        }
+    }
+}
