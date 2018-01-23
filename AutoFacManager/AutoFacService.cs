28328ef1edc0d20222e1caaf22bb90572da24da9
StarrySkyPfw <starrysky_pfw@qq.com>
2018-01-23 11:36:38 +0800

    AutoFac

---
+++ AutoFacManager/AutoFacService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Autofac;
+
+namespace AutoFacManager
+{
+    public class AutoFacService
+    {
+        static IContainer container = null;
+
+        public static ContainerBuilder builder = null;
+        public static IComponentContext context = null;
+
+
+        static AutoFacService()
+        {
+            if (builder == null)
+            {
+                builder = new ContainerBuilder();
+            }
+        }
+
+
+        public IComponentContext AutoFacContainer()
+        {
+            if (context == null)
+            {
+                context = builder.Build();
+            }
+            return container;
+        }
+
+
+    }
+}
