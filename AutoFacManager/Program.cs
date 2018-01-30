345d0bfee5ccf745d5abb919d2e39106a0c7e2d3
StarrySkyPfw <starrysky_pfw@qq.com>
2018-01-30 11:00:05 +0800

    Autofac

---
+++ AutoFacManager/Program.cs
@@ -0,0 +1,20 @@
+﻿using AutoFacServer;
+using System;
+using Autofac;
+
+namespace AutoFacManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = AutoFacService.AutoFacContainer();
+
+            var rst = container.ResolveNamed<BaseInterface>("BaseInterface");
+            var rst1 = container.ResolveNamed("BaseInterface", typeof(BaseInterface));
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- AutoFacServer/AutoFacService.cs
+++ AutoFacServer/AutoFacService.cs
@@ -1,15 +1,13 @@
-﻿using System;
-using Autofac;
+﻿using Autofac;
 using Autofac.Core;
+using System;
 
-namespace AutoFacManager
+namespace AutoFacServer
 {
     public class AutoFacService
     {
         static IContainer container = null;
-
         public static ContainerBuilder builder = null;
-        public static IComponentContext context = null;
 
 
         static AutoFacService()
@@ -18,16 +16,16 @@ namespace AutoFacManager
             {
                 builder = new ContainerBuilder();
             }
-            builder.RegisterModule<InterfaceModule>();
+            //builder.RegisterModule<InterfaceModule>();
             builder.RegisterModule(new InstanceModule());
         }
 
 
-        public IComponentContext AutoFacContainer()
+        public static IComponentContext AutoFacContainer()
         {
-            if (context == null)
+            if (container == null)
             {
-                context = builder.Build();
+                container = builder.Build();
             }
             return container;
         }
@@ -46,9 +44,42 @@ namespace AutoFacManager
     {
         protected override void Load(ContainerBuilder builder)
         {
+            builder.RegisterType<BaseOne>().Named<BaseInterface>("BaseInterface");
+            builder.RegisterType<BaseTwo>().Named<BaseInterface>("BaseInterface");
+            builder.RegisterType<BaseThree>().Named<BaseInterface>("BaseInterface");
             base.Load(builder);
         }
     }
 
 
+    public interface BaseInterface
+    {
+        void Load();
+    }
+
+    public class BaseOne : BaseInterface
+    {
+        public void Load()
+        {
+            Console.WriteLine("BaseoOne");
+        }
+    }
+
+
+    public class BaseTwo : BaseInterface
+    {
+        public void Load()
+        {
+            Console.WriteLine("BaseTwo");
+        }
+    }
+
+
+    public class BaseThree : BaseInterface
+    {
+        public void Load()
+        {
+            Console.WriteLine("BaseThree");
+        }
+    }
 }
