0b3ccbe237d9103667ebd4be797d6eb6a480a968
StarrySkyPfw <starrysky_pfw@qq.com>
2018-02-06 18:03:17 +0800

    redis grammer

--- RedisClient/Program.cs
+++ RedisClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿using RedisService;
+using StackExchange.Redis;
 using System;
 
 namespace RedisClient
@@ -7,9 +8,19 @@ namespace RedisClient
     {
         static void Main(string[] args)
         {
+            #region string： set与get
             RedisCacheProvider.Database().StringSet("name", "pfw");
             Console.WriteLine(RedisCacheProvider.Database().StringGet("name"));
-            var points = RedisCacheProvider.Multiplexer().GetEndPoints();
+            #endregion
+
+            #region hash  get与set
+            RedisCacheProvider.Database().HashSet("userinfo", "name", "pfw");
+            RedisCacheProvider.Database().HashSet("userinfo", "age", "24");
+
+            var hashEntrys = new HashEntry[2] { new HashEntry("username", "starrysky"), new HashEntry("userage", "25") };
+            RedisCacheProvider.Database().HashSet("userinfo", hashEntrys);
+            var minfo = RedisCacheProvider.Database().HashGetAll("userinfo");
+            #endregion
 
             Console.WriteLine("Hello World!");
             Console.ReadKey();
--- RedisService/RedisCacheProvider.cs
+++ RedisService/RedisCacheProvider.cs
@@ -3,22 +3,36 @@ using StackExchange.Redis;
 
 namespace RedisService
 {
-    public class RedisCacheProvider
+    public sealed class RedisCacheProvider
     {
-        static ConnectionMultiplexer connectionMultiplexer;
-
-        public static ConnectionMultiplexer Multiplexer()
-        {
-            if (connectionMultiplexer == null || !connectionMultiplexer.IsConnected)
-            {
-                connectionMultiplexer = ConnectionMultiplexer.Connect(new ConfigurationOptions() { EndPoints = { { "127.0.0.1", 6379 }, { "192.168.0.13", 6379 } } }); //, Password = "pfwredis" 
-            }
-            return connectionMultiplexer;
-        }
+        static ConnectionMultiplexer connectionMultiplexer = ConnectionMultiplexer.Connect(new ConfigurationOptions() { EndPoints = { { "127.0.0.1", 6379 }, { "192.168.0.13", 6379 } } }); //, Password = "pfwredis" 
 
         public static IDatabase Database(int db = -1)
         {
-            return Multiplexer().GetDatabase(db);
+            return connectionMultiplexer.GetDatabase(db);
+        }
+    }
+
+    public class StructDemo
+    {
+        public void Num()
+        {
+            StuctProvider provider = new StuctProvider();
+            provider.X = 25;
+        }
+    }
+
+    /// <summary>
+    /// 结构
+    /// 结构是值类型 存储在堆栈上 性能高   多用于值计算
+    /// </summary>
+    public struct StuctProvider
+    {
+        public int X;
+
+        public static Int64 GetNum(int num)
+        {
+            return num * 10;
         }
     }
 }
